b1db8b4a48dde553b0ee77ff2c46b0156d31a358
Ruan Petrus <xastroboyx11@gmail.com>
2022-02-17 19:04:05 -0300

    blabla

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = "dracula"
+local colorscheme = "nord"
 
 vim.g.nord_contrast = true
 vim.g.nord_borders = false
---
+++ nvim/.config/nvim/lua/user/lsp/settings/jdtls.lua
@@ -0,0 +1,6 @@
+return {
+    init_options = {
+          jvm_args = {"-javaagent:/usr/local/share/lombok/lombok.jar"},
+          workspace = "/home/runner/workspace"
+    },
+}
