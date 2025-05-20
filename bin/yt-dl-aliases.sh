e24483e92d6f307eecdd549e93fb52171bdf0ff7
RuanPetrus <xastroboyx11@gmail.com>
2025-05-20 10:22:20 -0300

    Changing status bar

---
+++ bin/yt-dl-aliases.sh
@@ -0,0 +1,9 @@
+# youtube-dl alias
+mp3 () {
+    yt-dlp --embed-thumbnail --add-metadata -f bestaudio --extract-audio --audio-format mp3 -o '~/Music/yt-dl/%(title)s.%(ext)s' "$1"
+}
+
+mp3p () {
+
+    yt-dlp --sleep-interval 3 --embed-thumbnail --add-metadata -f bestaudio --extract-audio --audio-format best -o '~/Music/yt-dl/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' "$1"
+}
