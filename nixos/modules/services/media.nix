3a200f42886d2262dfa124fa078e2b230b53ff57
RuanPetrus <xastroboyx11@gmail.com>
2026-08-24 12:18:01 -0300

    Making arr stack more declarative

--- nixos/modules/services/media.nix
+++ nixos/modules/services/media.nix
@@ -12,11 +12,136 @@
 
     jellyfin.enable = true;
     transmission.enable = true;
-    bazarr.enable = true;
+    bazarr = {
+      enable = true;
+      settings-sync = {
+        sonarr.enable = true;
+        radarr.enable = true;
+      };
+    };
     lidarr.enable = true;
-    prowlarr.enable = true;
-    radarr.enable = true;
-    sonarr.enable = true;
+    prowlarr = {
+      enable = true;
+      settings-sync = {
+        enable-nixarr-apps = true;
+        indexers = [
+          {
+            sort_name = "limetorrents";
+            name = "LimeTorrents";
+            priority = 25;
+            fields = {
+              definitionFile = "limetorrents";
+              "baseSettings.limitsUnit" = 0;
+              "torrentBaseSettings.preferMagnetUrl" = false;
+              primarydownloadlink = 1;
+              fallbackdownloadlink = 0;
+              sort = 0;
+            };
+          }
+          {
+            sort_name = "nyaa si";
+            name = "Nyaa.si";
+            priority = 25;
+            fields = {
+              definitionFile = "nyaasi";
+              "baseSettings.limitsUnit" = 0;
+              "torrentBaseSettings.preferMagnetUrl" = false;
+              prefer_magnet_links = true;
+              sonarr_compatibility = false;
+              strip_s01 = false;
+              radarr_compatibility = false;
+              "filter-id" = 0;
+              "cat-id" = 0;
+              sort = 0;
+              type = 1;
+            };
+          }
+          {
+            sort_name = "pirate bay";
+            name = "The Pirate Bay";
+            priority = 25;
+            fields = {
+              definitionFile = "thepiratebay";
+              "baseSettings.limitsUnit" = 0;
+              "torrentBaseSettings.preferMagnetUrl" = false;
+              apiurl = "apibay.org";
+              top100 = 6;
+            };
+          }
+        ];
+      };
+    };
+    radarr = {
+      enable = true;
+      settings-sync.transmission = {
+        enable = true;
+        config = {
+          priority = 1;
+          fields = {
+            urlBase = "/transmission/";
+            movieCategory = "radarr";
+          };
+        };
+      };
+    };
+    sonarr = {
+      enable = true;
+      settings-sync.transmission = {
+        enable = true;
+        config = {
+          priority = 1;
+          fields = {
+            urlBase = "/transmission/";
+            tvCategory = "tv-sonarr";
+          };
+        };
+      };
+    };
+    recyclarr = {
+      enable = true;
+      schedule = "daily";
+      configuration = {
+        radarr.movies = {
+          base_url = "http://127.0.0.1:7878";
+          api_key = "!env_var RADARR_API_KEY";
+          delete_old_custom_formats = true;
+          quality_definition.type = "movie";
+          quality_profiles = [
+            {
+              trash_id = "d1d67249d3890e49bc12e275d989a7e9"; # HD Bluray + WEB
+              reset_unmatched_scores.enabled = true;
+            }
+          ];
+          custom_format_groups.add = [
+            { trash_id = "f8bf8eab4617f12dfdbd16303d8da245"; } # Golden Rule HD
+            { trash_id = "a3ac6af01d78e4f21fcb75f601ac96df"; } # Unwanted Formats
+          ];
+        };
+
+        sonarr.series = {
+          base_url = "http://127.0.0.1:8989";
+          api_key = "!env_var SONARR_API_KEY";
+          delete_old_custom_formats = true;
+          quality_definition.type = "series";
+          quality_profiles = [
+            {
+              trash_id = "72dae194fc92bf828f32cde7744e51a1"; # WEB-1080p
+              reset_unmatched_scores.enabled = true;
+            }
+            {
+              trash_id = "20e0fc959f1f1704bed501f23bdae76f"; # Anime Remux-1080p
+              reset_unmatched_scores.enabled = true;
+            }
+          ];
+          custom_format_groups.add = [
+            { trash_id = "158188097a58d7687dee647e04af0da3"; } # Golden Rule HD
+            { trash_id = "74aff4168620ed49dcc67e92b2c2a5b4"; } # Language Profiles
+            { trash_id = "85fae4a2294965b75710ef2989c850eb"; } # Streaming Services HD/UHD boost
+            { trash_id = "59c3af66780d08332fdc64e68297098f"; } # Unwanted Formats
+          ];
+        };
+      };
+    };
     seerr.enable = true;
   };
 
@@ -43,6 +168,7 @@
         "render"
       ];
       prowlarr.extraGroups = [ "media" ];
+      recyclarr.extraGroups = [ "media" ];
       ruan.extraGroups = [ "media" ];
       seerr.extraGroups = [ "media" ];
     };
@@ -61,6 +187,11 @@
   ];
 
   services = {
+    lidarr.settings.auth.required = "DisabledForLocalAddresses";
+    prowlarr.settings.auth.required = "DisabledForLocalAddresses";
+    radarr.settings.auth.required = "DisabledForLocalAddresses";
+    sonarr.settings.auth.required = "DisabledForLocalAddresses";
+
     samba = {
       enable = true;
       openFirewall = true;
