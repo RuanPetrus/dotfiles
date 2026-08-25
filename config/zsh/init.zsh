33be522e75f593fa312824fb19525b6f169f0924
RuanPetrus <xastroboyx11@gmail.com>
2026-08-25 12:37:08 -0300

    Ricing shell

--- config/zsh/init.zsh
+++ config/zsh/init.zsh
@@ -1,3 +1,6 @@
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 setopt AUTO_MENU COMPLETE_IN_WORD
+
+autoload -Uz colors && colors
+PROMPT='%F{green}%n@%m%f %F{blue}%~%f# '
--- nixos/modules/users/ruan/home.nix
+++ nixos/modules/users/ruan/home.nix
@@ -24,6 +24,11 @@
     ];
   };
 
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
