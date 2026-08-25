b88469bf8f740d5f371ba87f1b78b44533d7035a
RuanPetrus <xastroboyx11@gmail.com>
2026-08-25 18:24:05 -0300

    Creating dev shells

---
+++ dev-shells/cpp/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "C and C++ development shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          clang-tools
+          cmake
+          gcc
+          gdb
+          git
+          ninja
+          pkg-config
+        ];
+      };
+    };
+}
